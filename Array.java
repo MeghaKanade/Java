79a1b423637a7511d3f17baa30618bffcc696475
MeghaKanade <meghakanade5422@gmail.com>
2022-07-25 05:01:24 +0000

    Print Array

---
+++ Array.java
@@ -0,0 +1,8 @@
+public class Array{
+    public static void main(String[] args){
+        int[] myarray={1,4,12,47};
+        for(int i=0;i<myarray.length;i++){
+        System.out.println(myarray[i]);
+        }
+    }
+}
